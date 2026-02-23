279b7193ace4c718534816b2b8f3d38e09328f6a
Jaime <jaimeespinalpr@gmail.com>
2026-02-23 18:37:36 -0500

    Translate Gary contact title to English

--- waitlist-config.php
+++ waitlist-config.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
 define('WAITLIST_ADMIN_EMAIL', 'register@united-wc.com');  // Where UWC receives submissions
 define('WAITLIST_CONTACT_EMAIL', 'info@united-wc.com');    // Shown to families in confirmation email
 define('WAITLIST_CONTACT_NAME', 'Gary Munch');
-define('WAITLIST_CONTACT_TITLE', 'Director de operaciones y fundador');
+define('WAITLIST_CONTACT_TITLE', 'Director of Operations and Founder');
 define('WAITLIST_CONTACT_PHONE', '302-528-2180');
 
 define('WAITLIST_FROM_NAME', 'United Wrestling Club');
